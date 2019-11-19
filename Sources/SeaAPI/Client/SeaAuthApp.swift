f06114f7f96e270bc53b88e973915b5a54621b1e
rinsuki <428rinsuki+git@gmail.com>
2019-11-19 09:09:24 +0900

    fix for linux

--- Sources/SeaAPI/Client/SeaAuthApp.swift
+++ Sources/SeaAPI/Client/SeaAuthApp.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public struct SeaAuthApp {
     public let baseUrl: URL
--- Sources/SeaAPI/Client/SeaUserCredential.swift
+++ Sources/SeaAPI/Client/SeaUserCredential.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public struct SeaUserCredential: Codable {
     let baseUrl: URL
