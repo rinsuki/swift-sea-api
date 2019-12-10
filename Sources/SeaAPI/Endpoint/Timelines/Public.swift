ee902c8cd5dffe47c610815b7728a7cde0ef3eec
rinsuki <428rinsuki+git@gmail.com>
2019-12-10 22:19:18 +0900

    add PublicTimeline API

---
+++ Sources/SeaAPI/Endpoint/Timelines/Public.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by user on 2019/12/10.
+//
+
+import Foundation
+
+extension SeaAPI {
+    public struct PublicTimeline: SeaAPIEndpoint, Codable {
+        public let endpoint: String = "/api/v1/timelines/public"
+        public var body: SeaAPIBody {
+            let params: [String: CustomStringConvertible?] = [
+                "count": count,
+                "sinceId": sinceId,
+                "maxId": maxId,
+                "search": search,
+            ]
+            return .query(params.compactMapValues({ $0?.description }))
+        }
+        public typealias Response = [SeaPost]
+        
+        var count: Int?
+        var sinceId: Int?
+        var maxId: Int?
+        var search: String?
+        
+        public init(count: Int?, sinceId: Int?, maxId: Int?, search: String?) {
+            self.count = count
+            self.sinceId = sinceId
+            self.maxId = maxId
+            self.search = search
+        }
+    }
+}
